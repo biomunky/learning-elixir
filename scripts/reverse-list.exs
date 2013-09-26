c5f3a79071c9a807637d0e86f390f38c30cd44e3
biomunky <biomunky@gmail.com>
2013-09-26 23:12:31 +0100

    changed sum function, added sum_no_acc for exercise in chp7.  Map function added and some other 'building' lists examples.

--- scripts/reverse-list.exs
+++ scripts/reverse-list.exs
@@ -8,13 +8,22 @@ defmodule Hack do
 		acc
 	end
 
-	def sum([h|t], acc) do
-		sum(t, h + acc)
-	end
+	def sum([h|t], acc // 0), do: sum(t, h + acc)
+	def sum([], acc), do: acc
 
-	def sum([], acc) do 
-		acc
-	end
+	def sum_no_acc([]), do: 0
+	def sum_no_acc([x|xs]), do: x+sum_no_acc(xs)
+	
+	def len([]), do: 0
+	def len([_x|xs]), do: 1 + len(xs)
+
+	def square([]), do: []
+	def square([x|xs]), do: [x*x| square xs]
+
+	# Hack.map([1,2,3,4], fn(i)->i+1 end)
+	def map([], func), do: []
+	def map([x|xs], func), do: [func.(x) | map(xs, func)]
+
 end
 
 x = Hack.reverse_list [1,2,3], []
