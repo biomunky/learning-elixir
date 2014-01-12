963335d8603234c42b211fb796d93caaebccdeba
biomunky <biomunky@gmail.com>
2014-01-12 20:08:56 +0000

    exunit

---
+++ scripts/tests/drop_test.exs
@@ -0,0 +1,14 @@
+ExUnit.start
+
+defmodule DropTest do
+	use ExUnit.Case, async: true
+
+  test "Zero distance gives zero velocity" do
+    assert Drop.fall_velocity(:earth, 0) == 0
+  end
+
+  test "Mars works?" do
+    assert Drop.fall_velocity(:mars, 10) == :math.sqrt(2 * 3.71 * 10)
+  end
+
+end
