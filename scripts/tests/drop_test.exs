97a89b56c88a230018dc6e3403fba6408b09f14c
biomunky <biomunky@gmail.com>
2014-01-12 20:28:16 +0000

    raising errors

--- scripts/tests/drop_test.exs
+++ scripts/tests/drop_test.exs
@@ -11,4 +11,14 @@ defmodule DropTest do
     assert Drop.fall_velocity(:mars, 10) == :math.sqrt(2 * 3.71 * 10)
   end
 
+  test "Earth works?" do
+    calculated = Drop.fall_velocity(:earth, 1)
+    assert_in_delta calculated, 4.4, 0.05,
+                                       "Result of #{calculated} not within 0.05 of 4.4"
+  end
+
+  test "Unknown planet" do
+    assert_raise CaseClauseError, fn -> Drop.fall_velocity(:planetX, 10) end
+  end
+
 end
